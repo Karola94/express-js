73973c6ee0587d83befbd0a5346006ed7a423299
Karolina Kuchta <linka_94@interia.eu>
2019-11-19 15:01:16 +0100

    Contact form

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
 var app = express();
-var stringifyFile;
 
-app.use(bodyParser.json());
+app.use(express.static('assets'));
 
-app.get('/getNote', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /getNote');
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data
-        res.send(data);
-    });
+app.get('/', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony głównej');    
+    res.sendFile('/index.html');
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony /updateNote/:note');
-    stringifyFile += req.params.note;
-    fs.writeFile('./test.json', stringifyFile, function(err) {        
-        if (err) throw err;        
-        console.log('file updated');
-    });
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.json(response);
 });
 
-app.listen(3000);
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
