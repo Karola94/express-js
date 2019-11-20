a46566f6a78f90bd676ce2025280dfb1f0148363
Karolina Kuchta <linka_94@interia.eu>
2019-11-20 11:43:27 +0100

    Add google authentication

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '536101321538-1v6ffou3t7k5drra86moakrco6t78oep.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'zO-O_i9ElqXg6XRMKbgBNQEO',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,37 +1,71 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
+));
 
 app.set('view engine', 'pug');
-app.set('views','./views');
-app.use('/views/google', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /google');
-    next();
+app.set('views', './auth');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function(req, res){
+    res.render('auth/content', { user: req.user });
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+app.get('/auth/google', function(req, res){
+    res.render('google', { user: googleProfile });
 });
 
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
-});
-
-app.get('/first-template', function(req, res){
-    res.render('first-template');
-});
-
-app.get('/views', function(req, res){
-    res.render('content', {
-        url: "/views/google"
-    });
-});
-
-
-app.get('/views/google', function(req, res){
-    res.render('google');
-});
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
 app.listen(3000);
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
+
+
+
+
+
+// app.get('/views', function(req, res){
+//     res.render('content', {
+//         url: "/views/google"
+//     });
+// });
+
+// app.get('/views/google', function(req, res){
+//     res.render('google');
+// });
+
+// app.listen(3000);
+// app.use(function (req, res, next) {
+//     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+// });
